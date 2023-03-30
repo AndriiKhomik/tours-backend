dd8015586e82e0d88dc626d50c6149729b10d95e
Andrii <andrii.khomik@gmail.com>
2023-03-30 13:47:47 +0300

    expand user model

--- models/User.js
+++ models/User.js
@@ -1,25 +1,32 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    roles: {
+      type: [String],
+      default: ["User"],
+    },
+    foundedDate: {
+      type: String,
+      required: true,
+    },
+    albums: {
+      type: [String],
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  foundedDate: {
-    type: String,
-    required: true,
-  },
-  albums: {
-    type: [String],
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
